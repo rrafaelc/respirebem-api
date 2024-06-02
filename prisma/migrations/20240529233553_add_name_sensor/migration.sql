13f2d21449fc5ce56632db44ed5b36bffb24646a
Rafael Costa <rafaelcosta8020@gmail.com>
2024-06-02 08:46:39 -0300

    feat: add config

---
+++ prisma/migrations/20240529233553_add_name_sensor/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Sensor" ADD COLUMN     "name" TEXT;
--- src/routes/root.ts
+++ src/routes/root.ts
@@ -1,8 +1,11 @@
 import { FastifyPluginAsync } from 'fastify'
+import { prisma } from '../lib/prisma';
 
 const root: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
+  const user = await prisma.user.findMany();
+
   fastify.get('/api', async function (request, reply) {
-    return { root: true, teste: "teste" }
+    return { root: true, user }
   })
 }
 
