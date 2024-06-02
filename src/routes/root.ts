e949d2caea156480384ea5898181590d5e8d9257
Rafael Costa <rafaelcosta8020@gmail.com>
2024-06-01 21:41:55 -0300

    testing

--- src/routes/root.ts
+++ src/routes/root.ts
@@ -2,7 +2,7 @@ import { FastifyPluginAsync } from 'fastify'
 
 const root: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
   fastify.get('/api', async function (request, reply) {
-    return { root: true }
+    return { root: true, teste: "teste" }
   })
 }
 
