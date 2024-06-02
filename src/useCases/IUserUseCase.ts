4ac3b890c545f4010e2edc061bdddef285b6dbc7
Rafael Costa <rafaelcosta8020@gmail.com>
2024-06-02 16:28:36 -0300

    Delete src/useCases directory

--- src/useCases/IUserUseCase.ts
+++
@@ -1,6 +0,0 @@
-import { CreateUserDto } from '../dtos/createUser.dto';
-import { IUser } from '../interfaces/IUser';
-
-export interface IUserUseCase {
-  create(request: CreateUserDto): Promise<IUser>;
-}
